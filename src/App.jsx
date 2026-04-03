89ad7efc02c01e0b05d25fd1ef8da53169298bd9
aliaarabia98-droid <aliaarabia98@gmail.com>
2026-04-03 18:32:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,8 @@ import GitJob from './Pages/GitJob/GitJob';
 import Service from './Pages/Service/Service';
 import FindFreelancersPage from './Pages/FindFreelancersPage/FindFreelancersPage';
 import ProvidersProvider from './Components/Context/ProvidersContext';
- 
+import EmptyDashboard from './Pages/Dashboard/EmptyDashboard';
+
  
  
  
@@ -119,7 +120,10 @@ function App() {
   path: "find-freelancers-page",
   element: <FindFreelancersPage/>
 },
-   
+ {
+path: "Dashboard",
+element: <EmptyDashboard/>
+},
    
               
               
